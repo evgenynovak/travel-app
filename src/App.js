3fb715908f55f3ab9f281b733e6ab2fc0c78b407
evgenynovak <evnovak171@mail.ru>
2021-03-12 23:11:32 +0300

    feat: add routing and start logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import React from 'react';
 import './App.scss';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import travelData from './travelData.js'; // данные для теста
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      test
+      <Header />
+        {/* 
+          Компонент СountryList map-ит
+            Компонент СountryCard
+
+          СountryCard при клике на него переход на большую страницу страны, компонент Сountry
+        */}
+
+      <Footer />
     </div>
   );
 }
 
-export default App;
+function LocalizeApp() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/ru">
+          <App lang="ru" />
+        </Route>
+        <Route path="/en">
+          <App lang="en" />
+        </Route>
+        <Route path="/de">
+          <App lang="de" />
+        </Route>
+        <Redirect from="/" to="/ru" />
+      </Switch>
+    </Router>
+  );
+}
+
+
+export default LocalizeApp;
+
---
+++ src/travelData.js
@@ -0,0 +1,14 @@
+let travelData = [
+  {
+    id: 1,
+    name: 'USA',
+    capital: 'Washington',
+    photo: 'usaPhoto',
+    info: 'usaInfo',
+    photoGallery: 'photoGallery',
+    video: 'usaVideo',
+    map:'usaMap', 
+  },
+]
+
+export default travelData;
